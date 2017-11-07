b50a40e6d40d735c05cd1817516725b9386f2ae6
jinmy <solisjinmy@gmail.com>
2017-11-07 20:12:15 +0100

    first commit

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+use Faker\Generator as Faker;
+use App\User;
+use App\Seller;
+use App\Product;
+use App\Category;
+use App\Transaction;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(App\User::class, function (Faker $faker) {
+    static $password;
+
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => $password ?: $password = bcrypt('secret'),
+        'remember_token' => str_random(10),
+        'verified' => $verificado = $faker->randomElement([User::USUARIO_VERIFICADO, User::USUARIO_NO_VERIFICADO]),
+        'verification_token' => $verificado == User::USUARIO_VERIFICADO ? null : User::generarVerificationToken(),
+        'admin' => $faker->randomElement([User::USUARIO_ADMINISTRADOR, User::USUARIO_REGULAR]),
+    ];
+});
